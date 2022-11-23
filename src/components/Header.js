c8343b409fa81377be3682a22769666230ca9e8f
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-11-23 15:08:27 -0800

    add navbar toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,42 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { useState } from 'react';
+import { Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 import AALogo from '../app/assets/img/aa_small.jpg';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <Navbar dark color='primary' sticky='top' expand='md'>
-            <NavbarBrand href='/'>
+            <NavbarBrand className='ms-5' href='/'>
                 <img src={AALogo} alt='Accordion Apocalypse logo' />
+                <h1 className='mt-1'>Accordion Apocalypse</h1>
             </NavbarBrand>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+            <Collapse isOpen={menuOpen} navbar>
+                <Nav className='ms-auto' navbar>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/'>
+                            <i className='fa fa-home fa-lg' /> Home
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/directory'>
+                            <i className='fa fa-list fa-lg' /> Accordions
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/about'>
+                            <i className='fa fa-info fa-lg' /> About
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/contact'>
+                            <i className='fa fa-address-card fa-lg' /> Contact
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
         </Navbar>
     )
 }
