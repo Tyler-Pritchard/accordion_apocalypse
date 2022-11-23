d1b8edc7ac86c583ce2f5f68d151f2f48902d4b9
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-11-23 12:13:52 -0800

    update index/app.js, import placeholder content

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+import 'font-awesome/css/font-awesome.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
