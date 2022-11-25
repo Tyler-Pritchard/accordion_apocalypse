2a448daae6aace1e27bc9f4922c131148e76b69a
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-11-25 14:23:28 -0800

    create instructors template data, import profile icon, setup files for display

---
+++ src/app/shared/INSTRUCTORS.js
@@ -0,0 +1,35 @@
+import profile_icon from '../assets/img/profile_icon.png';
+
+export const PROMOTIONS = [
+    {
+        id: 0,
+        name: 'Big Lou the Accordion Princess',
+        image: profile_icon,
+        featured: true,
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id: 1,
+        name: 'Marian Kelly',
+        image: profile_icon,
+        featured: false,
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id: 2,
+        name: 'Peter Di Bono',
+        image: profile_icon,
+        featured: false,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id: 3,
+        name: 'Allison Lovejoy',
+        image: profile_icon,
+        featured: false,
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+];
