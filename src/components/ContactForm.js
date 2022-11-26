e421c0d0a9d83e26fd133c35c6bf245d23d1f78a
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-11-25 22:00:37 -0800

    update contact form submit handler

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,6 +2,13 @@ import { Button, Label, Col, FormGroup} from 'reactstrap';
 import { Formik, Field, Form } from 'formik';
 
 const ContactForm = () => {
+    
+    const handleSubmit = (values, {resetForm}) => {
+        console.log('form values:', values);
+        console.log('in JSON format:', JSON.stringify(values));
+        resetForm();
+    }
+
     return (
         <Formik
             initialValues={{
@@ -13,6 +20,7 @@ const ContactForm = () => {
                 contactType: 'By Phone',
                 feedback: ''
             }}
+            onSubmit={handleSubmit}
         >
             <Form>
                 <FormGroup row>
@@ -77,7 +85,13 @@ const ContactForm = () => {
                     </Col>
                 </FormGroup>
 
-                <FormGroup row></FormGroup>
+                <FormGroup row>
+                    <Col md={{ size: 10, offset: 2}}>
+                        <Button type='submit' color='primary'>
+                            Submit Message
+                        </Button>
+                    </Col>
+                </FormGroup>
             </Form>
         </Formik>
     );
