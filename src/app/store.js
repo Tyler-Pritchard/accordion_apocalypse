222e8a44f52841556080f0c43f11458adb7a5efb
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-12-28 13:27:39 -0800

    add error & loading components, image URLs still bugged

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import { productsReducer } from '../features/products/productsSlice';
 import { eventsReducer } from '../features/events/eventsSlice';
--- src/features/products/ProductsList.js
+++ src/features/products/ProductsList.js
@@ -2,14 +2,34 @@ import { useSelector } from 'react-redux';
 import { Col, Row } from 'reactstrap';
 import ProductCard from './ProductCard';
 import { selectAllProducts } from './productsSlice';
+import Error from '../../components/Error'; 
+import Loading from '../../components/Loading';
 
 const ProductsList = () => {
     const products = useSelector(selectAllProducts);
     console.log('products:', products);
+    const isLoading = useSelector((state) => state.products.isLoading);
+    const errMsg = useSelector((state) => state.products.errMsg);
 
     return (
         <Row className='ms-auto'>
             {products.map((product) => {
+                if (isLoading) {
+                    return (
+                        <Row>
+                            <Loading />
+                        </Row>
+                    )
+                }
+
+                if (errMsg) {
+                    return (
+                        <Row>
+                            <Error errMsg={errMsg} />
+                        </Row>
+                    )
+                }
+
                 return (
                     <Col 
                         md='5' 
