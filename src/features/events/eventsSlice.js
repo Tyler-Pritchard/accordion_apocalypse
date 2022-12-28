6353a80e55bae31c2213fb1997dd478cf4521f63
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-12-28 14:32:11 -0800

    update events and instructors slice to use async

--- src/features/events/eventsSlice.js
+++ src/features/events/eventsSlice.js
@@ -1,13 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { EVENTS } from '../../app/shared/test_data/EVENTS';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+// import { EVENTS } from '../../app/shared/test_data/EVENTS';
+import { baseUrl } from '../../app/shared/baseUrl';
+import { mapImageURL } from '../../utils/mapImageURL';
 
 const initialState = {
-    eventsArray: EVENTS
+    eventsArray: [],
+    isLoading: true,
+    errMsg: ''
 };
+
+export const fetchEvents = createAsyncThunk(
+    'events',
+    async () => {
+        const response = await fetch(baseUrl + 'events');
+        if (!response.ok) {
+            return Promise.reject('Unable to fetch, status: ' + response.status);
+        }
+        const data = await response.json()
+        return data;
+    }
+);
 
 const eventsSlice = createSlice({
     name: 'events',
-    initialState
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [fetchEvents.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [fetchEvents.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = '';
+            state.eventsArray = mapImageURL(action.payload);
+        },
+        [fetchEvents.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = action.error ? action.error.message : 'Fetch failed';
+        }
+    }
 });
 
 export const eventsReducer = eventsSlice.reducer;
--- src/features/instructors/instructorsSlice.js
+++ src/features/instructors/instructorsSlice.js
@@ -1,13 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { INSTRUCTORS } from '../../app/shared/test_data/INSTRUCTORS';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import { INSTRUCTORS } from '../../app/shared/test_data/INSTRUCTORS';
+import { baseUrl } from '../../app/shared/baseUrl';
+import { mapImageURL } from '../../utils/mapImageURL';
 
 const initialState = {
-    instructorsArray: INSTRUCTORS
+    instructorsArray: [],
+    isLoading: true,
+    errMsg: ''
 };
 
+export const fetchInstructors = createAsyncThunk(
+    'instructors/fetchInstructors',
+    async () => {
+        const response = await fetch(baseUrl + 'instructors');
+        if (!response.ok) {
+            return Promise.reject('Unable to fetch, status: ' + response.status);
+        }
+        const data = await response.json();
+        return data;
+    }
+);
+
 const instructorsSlice = createSlice({
-        name: 'instructors',
-        initialState
+    name: 'instructors',
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [fetchInstructors.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [fetchInstructors.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = '';
+            state.instructorsArray = mapImageURL(action.payload);
+        },
+        [fetchInstructors.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = action.error ? action.error.message: 'Fetch failed';
+        }
+    }
 });
 
 export const instructorsReducer = instructorsSlice.reducer;
