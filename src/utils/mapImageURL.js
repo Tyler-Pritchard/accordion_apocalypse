f644ed51a5912f39171955ecf5c4fc26bbf7db31
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-12-28 13:10:04 -0800

    create Error component, image URLs still bugged

--- src/utils/mapImageURL.js
+++ src/utils/mapImageURL.js
@@ -2,6 +2,7 @@ import { baseUrl } from "../app/shared/baseUrl"
 
 export const mapImageURL = (arr) => {
     return arr.map((item) => {
+        console.log(baseUrl);
         return {
             ...item,
             image: baseUrl + item.image
