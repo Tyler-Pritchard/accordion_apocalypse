25335def3a7eb3318dca419564c56130b63773aa
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-12-28 13:52:18 -0800

    add error/loading messages to product detail, Image urls still bugged

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -5,17 +5,32 @@ import { selectProductById } from '../features/products/productsSlice';
 import ProductDetail from '../features/products/ProductDetail';
 import CommentsList from '../features/comments/CommentsList';
 import SubHeader from '../components/SubHeader';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
 
 const ProductDetailPage = () => {
     const { productId } = useParams();
     const product = useSelector(selectProductById(productId));
+    const isLoading = useSelector((state) => state.products.isLoading);
+    const errMsg = useSelector((state) => state.products.errMsg);
+    let content = null;
+
+    if (isLoading) {
+        content = <Loading />;
+    } else if (errMsg) {
+        content = <Error errMsg={errMsg} />;
+    } else content = (
+        <>
+            <ProductDetail product={product} />
+            <CommentsList productId={productId} />
+        </>
+    )
 
     return (
         <Container>
-            <SubHeader current={product.title} detail={true} />
+            {product && <SubHeader current={product.title} detail={true} />}
             <Row>
-                <ProductDetail product={product} />
-                <CommentsList productId={productId} />
+                {content}
             </Row>
         </Container>
     );
--- src/utils/mapImageURL.js
+++ src/utils/mapImageURL.js
@@ -2,7 +2,6 @@ import { baseUrl } from "../app/shared/baseUrl"
 
 export const mapImageURL = (arr) => {
     return arr.map((item) => {
-        console.log(baseUrl);
         return {
             ...item,
             image: baseUrl + item.image
